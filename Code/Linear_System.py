15237aded7272f13fe1ba4c731caabe37abc393a
MichaelTA <michaelallen0111@gmail.com>
2020-12-24 13:32:22 +0000

    Made the linear system work

--- Code/Linear_System.py
+++ Code/Linear_System.py
@@ -1,5 +1,6 @@
 from scipy.integrate import solve_ivp
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class LinearSystem:
@@ -8,28 +9,28 @@ class LinearSystem:
         self.__x_1_e = 0.75 * (d_length + mass * gravity * np.sin(theta))
         self.__x_2_e = 0
         self.__i_e = np.sqrt((mass * gravity * np.sin(theta)
-                             - k_spring * (self.__x_1_e - d_length))
+                              - k_spring * (self.__x_1_e - d_length))
                              / (-c_const)
-                             * (delta - self.__x_1_e)**2)
+                             * (delta - self.__x_1_e) ** 2)
         self.__v_e = self.__i_e * resistance
 
-        self.__d = (5 / (3 * mass)) * (2 * c_const * self.__i_e / (delta - self.__x_1_e)**2)
-        self.__f = (5 / (3 * mass)) * (2 * c_const * self.__i_e**2 / (delta - self.__x_1_e)**3 - k_spring)
+        self.__d = (5 / (3 * mass)) * (2 * c_const * self.__i_e / (delta - self.__x_1_e) ** 2)
+        self.__f = (5 / (3 * mass)) * (2 * c_const * self.__i_e ** 2 / (delta - self.__x_1_e) ** 3 - k_spring)
         self.__h = (5 / (3 * mass)) * b_damper
         self.__n = 1 / (ell_0 + ell_1 * np.exp(-alpha * (delta - self.__x_1_e)))
         self.__p = resistance * self.__n
 
-        self.__x_1_bar = None
-        self.__x_2_bar = None
-        self.__i_bar = None
+        self.__x_1_bar = 0.1
+        self.__x_2_bar = 10.
+        self.__i_bar = 1.
 
-    def move(self, v_bar, dt=50):
+    def move(self, v_bar=0, dt=1, num_points=1001):
         initial_values = (self.__x_1_bar, self.__x_2_bar, self.__i_bar)
         state_values = solve_ivp(lambda time, z:
                                  self.ball_dynamics(time, z, v_bar),
                                  [0, dt],
                                  initial_values,
-                                 t_eval=np.linspace(0, dt, 51))
+                                 t_eval=np.linspace(0, dt, num_points))
 
         final_state = state_values.y.T[-1]
         self.__x_1_bar = final_state[0]
@@ -44,4 +45,13 @@ class LinearSystem:
         return [x_1_bar_dot, x_2_bar_dot, i_bar_dot]
 
 
-steel_ball = LinearSystem()
+if __name__ == '__main__':
+    ball = LinearSystem()
+    ball_trajectory = ball.move(0)
+
+    plt.plot(ball_trajectory.t, ball_trajectory.y[0].T)
+    plt.xlabel('Time (s)')
+    plt.ylabel('$\overline{x}_1$ (m)')
+    plt.grid()
+    plt.savefig('.\\Figures\\linear_system.svg', format='svg')  # Save the graph as a .svg file
+    plt.show()
